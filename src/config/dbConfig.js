9f2df475703f6328567551ca3cd65b606dcd392d
Raj2710 <nagarajansai2727@gmail.com>
2024-01-24 11:36:10 +0530

    Express Mongo

---
+++ src/config/dbConfig.js
@@ -0,0 +1,7 @@
+const DB_URL = "mongodb+srv://Raj2710:Raj2710@raj.x3e0h.mongodb.net/test"
+const DB_NAME = "b53wdt_crud"
+
+export default {
+    DB_URL,
+    DB_NAME
+}
--- src/controller/user.js
+++ src/controller/user.js
@@ -1,116 +1,135 @@
 import {findIndex} from '../common/helper.js'
-const user = [{
-    id:1,
-    name:"Nagarajan",
-    email:"nag@gmail.com",
-    password:"123",
-    status:true,
-    role:"user"
-}]
+import DB_CONFIG from '../config/dbConfig.js'
+import mongodb,{MongoClient} from 'mongodb'
 
-const getAllUsers = (req,res)=>{
+const user = []
+const client =  new MongoClient(DB_CONFIG.DB_URL)
+
+const getAllUsers = async(req,res)=>{
+    await client.connect()
     try {
+        const db = await client.db(DB_CONFIG.DB_NAME)
+        let users = await db.collection('users').find().toArray()
         res.status(200).send({
             message:"User data fetch successful",
-            user
+            users
         })
     } catch (error) {
         res.status(500).send({
             message:"Internal Server Error"
         })
     }
+    finally{
+        client.close()
+    }
 }
-
-const getUserById = (req,res)=>{
-    try {
-        const {id} = req.params
-        let index = findIndex(user,id)
-        if(index!==-1)
-        {
+const getUserById = async(req,res)=>{
+        await client.connect()
+        try {
+            const db = await client.db(DB_CONFIG.DB_NAME)
+            let user = await db.collection('users').findOne({_id:new mongodb.ObjectId(req.params.id)})
             res.status(200).send({
                 message:"User data fetch successful",
-                user:user[index]
+                user
+            })
+        } catch (error) {
+            console.log(error)
+            res.status(500).send({
+                message:"Internal Server Error"
             })
         }
-        else
-        {
-            res.status(400).send({
-                message:"Invalid User Id",
-            })
+        finally{
+            client.close()
         }
-    } catch (error) {
-        res.status(500).send({
-            message:"Internal Server Error"
-        })
     }
-}
 
-const addUser = (req,res)=>{
+const addUser = async(req,res)=>{
+    await client.connect()
     try {
-        let id = user.length?user[user.length-1].id+1:1
-
-        req.body.id = id
-        
-        user.push(req.body)
-
-        res.status(200).send({
-            message:"User Added Successfully"
-        })
-    } catch (error) {
-        res.status(500).send({
-            message:"Internal Server Error"
-        })
-    }
-}
-
-const editUserById = (req,res)=>{
-    try {
-        const {id} = req.params
-        let index = findIndex(user,id)
-        
-        if(index!==-1)
+        const db = await client.db(DB_CONFIG.DB_NAME)
+        //check if the email exists in db
+        const user = await db.collection('users').findOne({email:req.body.email})
+        if(!user)
         {
-            req.body.id = Number(id)
-            user.splice(index,1,req.body)
+            //if email not found create the user
+            let newUser = await db.collection('users').insertOne(req.body)
             res.status(200).send({
-                message:"User Edited successful"
+                message:"User Added Successfully"
             })
         }
         else
         {
+            //if email is found respond error message
             res.status(400).send({
-                message:"Invalid User Id",
+                message:`User with ${req.body.email} already exists`
             })
-        }
+        }    
     } catch (error) {
         res.status(500).send({
             message:"Internal Server Error"
         })
+    }
+    finally{
+        client.close()
     }
 }
 
-const deleteUserById = (req,res)=>{
-    try {
-        const {id} = req.params
-        let index = findIndex(user,id)
-        if(index!==-1)
-        {
-            user.splice(index,1)
-            res.status(200).send({
-                message:"User Deleted Successfully",
+const editUserById = async(req,res)=>{
+    await client.connect()
+        try {
+            const db = await client.db(DB_CONFIG.DB_NAME)
+            let user = await db.collection('users').findOne({_id:new mongodb.ObjectId(req.params.id)})
+            if(user)
+            {
+                await db.collection('users').updateOne({_id:new mongodb.ObjectId(req.params.id)},{$set:req.body})
+                res.status(200).send({
+                    message:"User Edited Successfully"
+                })
+            }
+            else
+            {
+                res.status(400).send({
+                    message:`Invalid User Id`
+                })
+            }
+        } catch (error) {
+            console.log(error)
+            res.status(500).send({
+                message:"Internal Server Error"
             })
         }
-        else
-        {
-            res.status(400).send({
-                message:"Invalid User Id",
+        finally{
+            client.close()
+        }
+}
+
+const deleteUserById = async(req,res)=>{
+    await client.connect()
+        try {
+            const db = await client.db(DB_CONFIG.DB_NAME)
+            let user = await db.collection('users').findOne({_id:new mongodb.ObjectId(req.params.id)})
+            if(user)
+            {
+                await db.collection('users').deleteOne({_id:new mongodb.ObjectId(req.params.id)})
+                res.status(200).send({
+                    message:"User Deleted Successfully"
+                })
+            }
+            else
+            {
+                res.status(400).send({
+                    message:`Invalid User Id`
+                })
+            }
+        } catch (error) {
+            console.log(error)
+            res.status(500).send({
+                message:"Internal Server Error"
             })
         }
-    } catch (error) {
-        res.status(500).send({
-            message:"Internal Server Error"
-        })
-    }
+        finally{
+            client.close()
+        }
 }
 export default {
     getAllUsers,
